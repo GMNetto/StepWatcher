9c28956ed05712da86af00832e516c8d591f72ee
Gustavo <gustavo.m.netto@outlook.com>
2015-10-24 14:43:52 -0400

    First Version

--- StepWatcher.ino
+++ StepWatcher.ino
@@ -1,17 +1,18 @@
 
 
 
-
+#define MINIMUMULTRA 300
+#define MINIMUMIR 300
+#define SAMPLERATE 100
+#define TIMEVIBRATING 10
 
 int accelerometer_previousX = 0;
 int accelerometer_previousY = 0;
 int accelerometer_previousZ = 0;
-int accelerometer_mean = 4;
-int accelerometer_tolerance = 1;
-
 int led_Pin = 13;
 
 long sample_time = 0;
+long time_already_vibrating = 0;
 
 const int accelerometer_pinX = A0;
 const int accelerometer_pinY = A1;
@@ -23,47 +24,36 @@ const int motor_pin = 2;
 long timer = 0;
 
 void foot_on_ground();
-void (*state)() = foot_on_ground;
+void (*state)() = foot_in_air;
+
+void turn_on_motor(){
+  digitalWrite(motor_pin, HIGH);;
+}
+
+
+void turn_off_motor(){
+  digitalWrite(motor_pin, LOW);;
+}
 
 void check_sensors() {
-  
+  int ultrasonic_reading = analogRead(ultrasonic_pin);
+  int infrared_reading = analogRead(r_i_pin);
+  if(ultrasonic_reading < MINIMUMULTRA || infrared_reading < MINIMUMIR)
+    turn_on_motor();
+    time_already_vibrating = 0;
+  state = foot_in_air;
 }
 
-int read_accelerometer() {
-  //TODO
-}
-
-void foot_in_air(){
-  if(millis() - sample_time > 100){
-    int accelerometer_signalX = analogRead(accelerometer_pinX);
-    sample_time = millis();
-    if(abs(accelerometer_signalX - accelerometer_previousX) > 20 && accelerometer_signalX*accelerometer_previousX < 0){
-      digitalWrite(led_Pin, HIGH);
-      timer = millis();
-      state = foot_on_ground;
-    }
-    if(millis() - timer > 1000){
-      digitalWrite(led_Pin, LOW);
-    }
-    accelerometer_previousX = accelerometer_signalX;
-    delay(75);
-  }
-}
-
-void foot_on_ground() {
-  if(millis() - sample_time > 100){
+void foot_in_air() {
+  if(millis() - sample_time > SAMPLERATE){
     int accelerometer_signalX = analogRead(accelerometer_pinX);
     int accelerometer_signalY = analogRead(accelerometer_pinY);
     int accelerometer_signalZ = analogRead(accelerometer_pinZ);
     sample_time = millis();
+    if(millis() - time_already_vibrating > TIMEVIBRATING)
+      turn_off_motor();
     if(abs(accelerometer_signalZ - accelerometer_previousZ) > 10 && (accelerometer_signalZ > 700 || accelerometer_signalZ < 200)){
-      digitalWrite(led_Pin, HIGH);
-      //state = foot_in_air;
-      //Serial.println(accelerometer_signalZ);
-      timer = millis();
-    }
-    if(millis() - timer > 500){
-      digitalWrite(led_Pin, LOW);
+      state = check_sensors; 
     }
     accelerometer_previousX = accelerometer_signalX;
     accelerometer_previousY = accelerometer_signalY;
@@ -72,24 +62,16 @@ void foot_on_ground() {
 }
 
 void setup() {
-  // put your setup code here, to run once:
   accelerometer_previousX = analogRead(accelerometer_pinX);
   accelerometer_previousY = analogRead(accelerometer_pinY);
   accelerometer_previousZ = analogRead(accelerometer_pinZ);
   pinMode(led_Pin, OUTPUT);
+  pinMode(motor_pin, OUTPUT);
   digitalWrite(led_Pin, LOW);
+  digitalWrite(motor_pin, LOW);
   Serial.begin(9600);
 }
 
 void loop() {
   state();
-//  int readingX = analogRead(accelerometer_pinX);
-//  int readingY = analogRead(accelerometer_pinY);
-//  int readingZ = analogRead(accelerometer_pinZ);
-//  Serial.print(readingX);
-//  Serial.print(" ");
-//  Serial.print(readingY);
-//  Serial.print(" ");
-//  Serial.print(readingZ);
-//  Serial.println();
 }
